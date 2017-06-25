d8725152d84388518e90c1aa22c5de6766050b2d
PhracturedBlue <rc2012@pblue.org>
2017-06-25 15:06:44 -0700

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'asteriskvm_server',
   packages=find_packages(),
-  version = '0.2.0',
+  version = '0.2.1',
   description = 'The server portion of a client/server to interact with Asterisk voicemail mailboxes',
   long_description=open('README.rst').read(),
   author = 'PhracturedBlue',
